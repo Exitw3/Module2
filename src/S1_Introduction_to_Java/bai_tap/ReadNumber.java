bdcb17f7bbda32813f75379e7f83f7b58d87fdaa
Nguyen <nguyennp.htra.031004@gmail.com>
2022-10-12 19:39:34 +0700

    baitap

--- src/S1_Introduction_to_Java/bai_tap/ReadNumber.java
+++ src/S1_Introduction_to_Java/bai_tap/ReadNumber.java
@@ -2,137 +2,99 @@ package S1_Introduction_to_Java.bai_tap;
 
 import java.util.Scanner;
 
+import com.sun.org.apache.xpath.internal.objects.XString;
+
+import java.util.Scanner;
+
 public class ReadNumber {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
-        Scanner input = new Scanner(System.in);
-        System.out.print("Nhập số muốn đọc: ");
-        int number = input.nextInt();
-        int first = number / 100;
-        int second = (number % 100) / 10;
-        int third = (number % 100) % 10;
-        String result = null;
-        if (number < 10) {
-            zero2Nine(number);
-            return;
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Nhập số cần chuyển: ");
+        int number = scanner.nextInt();
+        String[] string =
+                {"One ", "Two ", "Three ", "Four ", "Five ", "Six ", "Seven ", "Eight ", "Nine "};
+        String[] string2 =
+                {"Ten ", "Eleven ", "Twelve ", "Thirteen ", "Fourteen ", "Fifteen ", "Sixteen ", "Seventeen ", "Eighteen ", "Nineteen "};
+        while (number >= 1000) {
+            System.out.println("Nhập lại số nhỏ hơn 1000: ");
+            number = scanner.nextInt();
         }
-        if (number >= 10 && number < 20) {
-            _10_to_19(third);
+        if (number == 0) {
+            System.out.print("Zero");
+        } else {
+            switch (number / 100) {
+                case 1:
+                    System.out.print("One hundred ");
+                    break;
+                case 2:
+                    System.out.print("Two hundred ");
+                    break;
+                case 3:
+                    System.out.print("Three hundred ");
+                    break;
+                case 4:
+                    System.out.print("Four hundred ");
+                    break;
+                case 5:
+                    System.out.print("Five hundred ");
+                    break;
+                case 6:
+                    System.out.print("Six hundred ");
+                    break;
+                case 7:
+                    System.out.print("Seven hundred ");
+                    break;
+                case 8:
+                    System.out.print("Eight hundred ");
+                    break;
+                case 9:
+                    System.out.print("Nine hundred ");
+                    break;
+                default:
+                    System.out.print("");
+            }
+            switch ((number % 100) / 10) {
+                case 0:
+                    System.out.print("");
+                    break;
+                case 1:
+                    for (int i = 0; i < string2.length; i++) {
+                        if (number % 10 == i) {
+                            System.out.println(string2[i]);
+                        }
+                    }
+                    break;
+                case 2:
+                    System.out.print("twenty ");
+                    break;
+                case 3:
+                    System.out.print("Thirty ");
+                    break;
+                case 4:
+                    System.out.print("Forty ");
+                    break;
+                case 5:
+                    System.out.print("Fifty ");
+                    break;
+                case 6:
+                    System.out.print("Sixty ");
+                    break;
+                case 7:
+                    break;
+                case 8:
+                    System.out.print("Eighty ");
+                    break;
+                case 9:
+                    System.out.print("Ninety ");
+                    break;
+            }
+            if ((number % 100) / 10 != 1) {
+                for (int i = 1; i <= string.length; i++) {
+                    if (number % 10 == i) {
+                        System.out.print(string[i - 1]);
+                    }
+                }
+            }
         }
-        if (number > 19 && number < 100) {
-            result = _20_to_99(second);
-            if (third != 0)
-                result+= " " +zero2Nine(third);
-            System.out.println(result);
-        }
-        if (number > 99) {
-
-        }
-    }
-
-    public static String _20_to_99(int number) {
-        String second = null;
-        switch (number) {
-            case 2:
-                second = "Twenty";
-                break;
-            case 3:
-                second = "Thirty";
-                break;
-            case 4:
-                second = "Forty";
-                break;
-            case 5:
-                second = "Fifty";
-                break;
-            case 6:
-                second = "Sixty";
-                break;
-            case 7:
-                second = "Seventy";
-                break;
-            case 8:
-                second = "Eighty";
-                break;
-            case 9:
-                second = "Ninety";
-                break;
-        }
-
-        return second;
-    }
-
-    public static String _10_to_19(int number) {
-        String special = null;
-        switch (number) {
-            case 0:
-                special = "Ten";
-                break;
-            case 1:
-                special = "Eleven";
-                break;
-            case 2:
-                special = "Twelve";
-                break;
-            case 3:
-                special = "Thirteen";
-                break;
-            case 4:
-                special = "Fourteen";
-                break;
-            case 5:
-                special = "Fifteen";
-                break;
-            case 6:
-                special = "Sixteen";
-                break;
-            case 7:
-                special = "Seventeen";
-                break;
-            case 8:
-                special = "Eighteen";
-                break;
-            case 9:
-                special = "Nineteen";
-                break;
-        }
-        return special;
-    }
-
-    public static String zero2Nine(int number) {
-        String donviStr = null;
-        switch (number) {
-            case 0:
-                donviStr = "Zero";
-                break;
-            case 1:
-                donviStr = "One";
-                break;
-            case 2:
-                donviStr = "Two";
-                break;
-            case 3:
-                donviStr = "Three";
-                break;
-            case 4:
-                donviStr = "Four";
-                break;
-            case 5:
-                donviStr = "Five";
-                break;
-            case 6:
-                donviStr = "Six";
-                break;
-            case 7:
-                donviStr = "Seven";
-                break;
-            case 8:
-                donviStr = "Eight";
-                break;
-            case 9:
-                donviStr = "Nine";
-                break;
-        }
-        return donviStr;
     }
 }
