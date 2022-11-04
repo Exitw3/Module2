307f60d92d359fe795ee815035d14c662a0d58f2
Nguyen <nguyennp.htra.031004@gmail.com>
2022-11-04 11:58:32 +0700

    baitap

--- src/S7_AbtractAndInterface/thuc_hanh/interface_edible/AbstractAndInterfaceTests.java
+++ src/S7_AbtractAndInterface/thuc_hanh/interface_edible/AbstractAndInterfaceTests.java
@@ -1,25 +1,23 @@
 package S7_AbtractAndInterface.thuc_hanh.interface_edible;
 
-import S7_AbtractAndInterface.thuc_hanh.interface_edible.edible.Edible;
-
-public class AbstractAndInterfaceTests {
-    public static void main(String[] args) {
-        Animal[] animals = new Animal[2];
-        animals[0] = new Tiger();
-        animals[1] = new Chicken();
-        Fruit[] fruit = new Fruit[2];
-        fruit[0] = new Orange();
-        fruit[1] = new Apple();
-        for(Fruit fruits : fruit){
-            System.out.println(fruits.howToEat());
-        }
-        for (Animal animal : animals) {
-            System.out.println(animal.makeSound());
-
-            if (animal instanceof Chicken) {
-                Edible edible = (Chicken) animal;
-                System.out.println(edible.howToEat());
-            }
-        }
-    }
-}
+//public class AbstractAndInterfaceTests {
+//    public static void main(String[] args) {
+//        Animal[] animals = new Animal[2];
+//        animals[0] = new Tiger();
+//        animals[1] = new Chicken();
+//        Fruit[] fruit = new Fruit[2];
+//        fruit[0] = new Orange();
+//        fruit[1] = new Apple();
+//        for(Fruit fruits : fruit){
+//            System.out.println(fruits.howToEat());
+//        }
+//        for (Animal animal : animals) {
+//            System.out.println(animal.makeSound());
+//
+//            if (animal instanceof Chicken) {
+//                Edible edible = (Chicken) animal;
+//                System.out.println(edible.howToEat());
+//            }
+//        }
+//    }
+//}
--- src/S7_AbtractAndInterface/thuc_hanh/interface_edible/Apple.java
+++ src/S7_AbtractAndInterface/thuc_hanh/interface_edible/Apple.java
@@ -1,8 +1,8 @@
 package S7_AbtractAndInterface.thuc_hanh.interface_edible;
 
-public class Apple extends Fruit{
-    @Override
-    public String howToEat(){
-        return "Apple could be slided";
-    }
-}
+//public class Apple extends Fruit{
+//    @Override
+//    public String howToEat(){
+//        return "Apple could be slided";
+//    }
+//}
