d6a6894be5e9d11bff9ab0af2023dd871a1d868b
Nguyen <nguyennp.htra.031004@gmail.com>
2022-11-04 13:11:59 +0700

    baitap

---
+++ src/S12_JavaCollectionFramework/thuc_hanh/MyLinkedListQueue/Node.java
@@ -0,0 +1,11 @@
+package S12_JavaCollectionFramework.thuc_hanh.MyLinkedListQueue;
+
+class Node {
+    public int key;
+    public Node next;
+
+    public Node(int key) {
+        this.key = key;
+        this.next = null;
+    }
+}
