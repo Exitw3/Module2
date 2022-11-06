41b44c2d2dd8085cdf37b16ad0ade1ac821798c6
Nguyen <nguyennp.htra.031004@gmail.com>
2022-11-06 17:05:44 +0700

    baitap

---
+++ src/S18_Threading/thuc_hanh/UD_DemSo/Count.java
@@ -0,0 +1,28 @@
+package S18_Threading.thuc_hanh.UD_DemSo;
+
+public class Count implements Runnable{
+    private Thread myThread;
+
+    public Count() {
+        myThread = new Thread(this,"my runable thread");
+        System.out.println("my thread created" + myThread);
+        myThread.start();
+    }
+
+    public Thread getMyThread() {
+        return myThread;
+    }
+
+    @Override
+    public void run() {
+        try {
+            for (int i = 0; i < 10; i++) {
+                System.out.println("Printing the count " + i);
+                Thread.sleep(1000);
+            }
+        } catch (InterruptedException e) {
+            System.out.println("My thread interrupted");
+        }
+        System.out.println("My thread run is over");
+    }
+}
