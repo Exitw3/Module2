6f6ee81ef639150182f57f3a9617c99bb8aa55f7
Nguyen <nguyennp.htra.031004@gmail.com>
2022-11-06 16:37:08 +0700

    baitap

---
+++ src/S16_IOTextFile/thuc_hanh/CopyFileText/TestReadWrite.java
@@ -0,0 +1,20 @@
+package S16_IOTextFile.thuc_hanh.CopyFileText;
+
+import java.util.List;
+
+public class TestReadWrite {
+
+    public static int countCharactor(List<String> list) {
+        int count = 0;
+        for (int i = 0; i < list.size(); i++) {
+            count += list.get(i).length();
+        }
+        return count;
+    }
+    public static void main(String[] args) {
+        ReadAndWriteFile readAndWriteFile = new ReadAndWriteFile();
+        List<String> list = readAndWriteFile.readFile("C:\\GitHub\\Module-2\\16. IO text file\\io_text_file\\src\\copy_file_text\\string.txt");
+        list.add(String.valueOf(countCharactor(list)));
+        readAndWriteFile.writeFile("C:\\GitHub\\Module-2\\16. IO text file\\io_text_file\\src\\copy_file_text\\result.txt",list);
+    }
+}
